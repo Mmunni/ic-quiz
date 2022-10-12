10d656ef7628c4fb803642de791dcba44bd53c51
Mmunni <masumamunni20@gmail.com>
2022-10-12 03:22:45 -0700

    question-number-added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,19 +3,18 @@ import './Question.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 
-const Question = ({qz}) => {
+const Question = ({qz, qzNo}) => {
     const {question,options} =qz
-    let index = 1;
     return (
         <div>
             <div className="question-container">
             <div className="question-area mb-4">
             <button className='correct-answer ms-auto'><FontAwesomeIcon icon={faEye} /></button>
-           <h4>{index ++} Q. {question}</h4>
+           <h4> Q. {qzNo} {question}</h4>
            </div>
            <div className="options">
            {
-            options.map(option =>  <div className="option">
+            options.map((option) =>  <div className="option">
             <input type="radio" id={option} name="option" value={option}/>
             <label for={option} className='ms-2'> {options}</label>
             </div>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -13,8 +13,9 @@ const Quiz = () => {
            <h2 className='mb-5'>Quiz of: {quiz.data.name}</h2>
            <div className="single-quiz">
             {
-                quiz.data.questions.map(qz => <Question 
+                quiz.data.questions.map((qz, index) => <Question 
                     key={qz.id}
+                    qzNo = {index + 1}
                     qz={qz}
                     ></Question>)
             }
