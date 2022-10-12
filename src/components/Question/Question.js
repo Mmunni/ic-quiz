7ef47e470f0da79c185759033c927f0acd6c8b5b
Mmunni <masumamunni20@gmail.com>
2022-10-12 16:37:22 -0700

    toast-updaetd

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -9,21 +9,22 @@ import 'react-toastify/dist/ReactToastify.css';
 const Question = ({qz, qzNo}) => {
     const {question,options, correctAnswer} =qz
     const correctAnswerHandeler = (correctAnswer) => {
-        toast(`Correct Answer: ${correctAnswer}`, {
+        toast(`Correct Answer is: ${correctAnswer}`, {
             position: toast.POSITION.TOP_CENTER
         });
     }
     const quizOptionHandeler = (option) => {
         if(option === correctAnswer) {
-            toast.success('your answer is correct', {
+            toast.success('Your Answer is Correct', {
                 position: toast.POSITION.TOP_CENTER
             });
         }
         else{
-           toast.error('your answer is wrong', {
+           toast.error('Your Answer is Wrong', {
                position: toast.POSITION.TOP_CENTER
            });
         }
+
     }
     return (
         <div>
@@ -31,12 +32,12 @@ const Question = ({qz, qzNo}) => {
             <div className="question-area mb-4">
             <button onClick={() => correctAnswerHandeler(correctAnswer)} className='correct-answer ms-auto'><FontAwesomeIcon icon={faEye} /></button>
             <ToastContainer></ToastContainer>
-           <h4> Q. {qzNo} {question}</h4>
+           <h4> Q. {qzNo} {question.slice(3,-4)}</h4>
            </div>
            <div className="options">
            {
-            options.map((option) =>  <div onClick={() => quizOptionHandeler(option)} className="option">
-            <input type="radio" id={option} name="option" />
+            options.map((option) =>  <div  className="option">
+            <input onClick={() => quizOptionHandeler(option)} type="radio" id={option} name="option" />
             <label for={option} className='ms-2'>{option}</label>
             <ToastContainer></ToastContainer>
             </div>
